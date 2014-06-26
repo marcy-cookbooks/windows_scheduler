cbb3464999715133bd4f7efbbf74f04fb5bfb3fa
Marcy <marcy9114@gmail.com>
2014-06-26 16:06:44 +0900

    Do not execute if not change using template resource

--- providers/job.rb
+++ providers/job.rb
@@ -9,22 +9,23 @@ action :create do
   powershell_script "Create job [#{new_resource.name}]" do
     cwd Chef::Config[:file_cache_path]
     code <<-EOH
-      $trigger = New-JobTrigger -Once -RepetitionInterval (New-TimeSpan -Minutes #{new_resource.span_minutes}) -RepetitionDuration (New-TimeSpan -Days #{new_resource.duration_days}) -At (Get-Date)
-      $option = New-ScheduledJobOption -MultipleInstancePolicy #{new_resource.multiple_policy}
-      $jobs = Get-ScheduledJob
-      $exist = 0
-      if ($jobs.Count -gt 0) {
-        for ($i=0; $i -lt $jobs.Count; $i++) {
-          if ($jobs[$i].Name -eq "#{new_resource.name}") {
-            Get-ScheduledJob -Name #{new_resource.name} | Set-ScheduledJob #{job} -Trigger $trigger -ScheduledJobOption $option
-            $exist = 1
-          }
-        }
-      }
-      if ($exist -eq 0) {
-        Register-ScheduledJob -Name #{new_resource.name} #{job} -Trigger $trigger -ScheduledJobOption $option
-      }
+      powershell.exe  -NoLogo -NonInteractive -NoProfile -ExecutionPolicy RemoteSigned -InputFormat None -File "C:\\opt\\chef\\windows_scheduler_job_#{new_resource.name}.ps1"
     EOH
+    action :nothing
+  end
+  template "C:\\opt\\chef\\windows_scheduler_job_#{new_resource.name}.ps1" do
+    source "job.ps1.erb"
+    mode "0644"
+    variables({
+     :span_minutes => new_resource.span_minutes,
+     :duration_days => new_resource.duration_days,
+     :multiple_policy => node[:powershell_cloudwatch][:region],
+     :name => new_resource.name,
+     :job => job
+    })
+    action :create
+    cookbook 'windows_scheduler'
+    notifies :run, "powershell_script[Create job [#{new_resource.name}]]"
   end
 end
 action :delete do
