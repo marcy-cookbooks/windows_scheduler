083575be2b2d1a59908f74e935b1638bf649a890
Marcy <marcy9114@gmail.com>
2014-06-26 10:28:35 +0900

    fix invalid resource attribute name

--- resources/job.rb
+++ resources/job.rb
@@ -3,5 +3,5 @@ default_action :create
 
 attribute :script, :kind_of => String, :default => nil
 attribute :script_path, :kind_of => String, :default => nil
-attribute :timespan_minutes, :kind_of => Integer, :required => true
+attribute :span_minutes, :kind_of => Integer, :required => true
 attribute :duration_days, :kind_of => Integer, :default => 8192
