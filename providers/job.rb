ef5be0a366549e36447f96ccdcef97634fa1586d
Marcy <marcy9114@gmail.com>
2014-06-26 16:10:00 +0900

    fix invalid path

--- providers/job.rb
+++ providers/job.rb
@@ -9,11 +9,11 @@ action :create do
   powershell_script "Create job [#{new_resource.name}]" do
     cwd Chef::Config[:file_cache_path]
     code <<-EOH
-      powershell.exe  -NoLogo -NonInteractive -NoProfile -ExecutionPolicy RemoteSigned -InputFormat None -File "C:\\opt\\chef\\windows_scheduler_job_#{new_resource.name}.ps1"
+      powershell.exe  -NoLogo -NonInteractive -NoProfile -ExecutionPolicy RemoteSigned -InputFormat None -File "C:\\chef\\windows_scheduler_job_#{new_resource.name}.ps1"
     EOH
     action :nothing
   end
-  template "C:\\opt\\chef\\windows_scheduler_job_#{new_resource.name}.ps1" do
+  template "C:\\chef\\windows_scheduler_job_#{new_resource.name}.ps1" do
     source "job.ps1.erb"
     mode "0644"
     variables({
